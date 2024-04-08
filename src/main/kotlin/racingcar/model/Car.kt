e302ea246c2aa9fbfcc688a25f6a8bac599d2f1e
younha00 <ayeon0@office.skhu.ac.kr>
2024-04-08 16:48:22 +0900

    Refactor: Car 도메인 생성자 수정 입력을 ","로 구분하는 코드 삭제

--- src/main/kotlin/racingcar/model/Car.kt
+++ src/main/kotlin/racingcar/model/Car.kt
@@ -11,9 +11,6 @@ class Car(
         require(name.length in MIN_NAME_LENGTH..MAX_NAME_LENGTH) {
             IllegalArgumentException("이름은 ${MIN_NAME_LENGTH}자 이상, ${MAX_NAME_LENGTH}자 이하만 가능합니다.")
         }
-        require(name.split(",").size == 1) {
-            IllegalArgumentException("이름은 쉼표로 구분되어야 합니다.")
-        }
     }
     fun moveForward() {
         distance++
