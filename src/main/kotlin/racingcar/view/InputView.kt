bf8356aa9c9745d9288a01034c06d4f1ef389ed9
younha00 <ayeon0@office.skhu.ac.kr>
2024-04-08 17:29:57 +0900

    Feat: 유저에게 입력을 받는 InputView 구현: * 참가하는 자동차의 이름을 입력 받음 * 몇 번 게임을 할 것 인지 입력 받음

---
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -0,0 +1,35 @@
+package racingcar.view
+import camp.nextstep.edu.missionutils.Console
+
+const val MESSAGE_OF_INPUT_CAR_NAMES: String = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,)를 기준으로 구분)"
+const val MESSAGE_OF_INPUT_GAME_TIME: String = "시도할 횟수는 몇 회인가요?"
+const val MINIMUM_NUMBER_OF_CARS: Int = 2
+
+
+class InputView {
+    fun inputCarNames(): List<String> {
+        println(MESSAGE_OF_INPUT_CAR_NAMES)
+        val cars: List<String> = Console.readLine().split(",")
+
+        require(cars.size > 1) {
+            IllegalArgumentException("자동차는 ${MINIMUM_NUMBER_OF_CARS}대 이상이어야 합니다.")
+        }
+        require(cars.distinct().size == cars.size) {
+            IllegalArgumentException("자동차 이름은 중복되면 안됩니다.")
+        }
+
+        return cars
+    }
+
+    fun inputGameTime(): Int {
+        println(MESSAGE_OF_INPUT_GAME_TIME)
+        val gameTime = Console.readLine().toInt()
+
+        require(gameTime > 0) {
+            IllegalArgumentException("게임 횟수는 1 이상의 정수여야 합니다.")
+        }
+
+        return gameTime
+    }
+
+}
