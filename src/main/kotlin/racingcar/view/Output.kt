1a4eaad0d0d7869e8b4cc69e32e49732becdabe6
cucumber99 <dlsguq1@gmail.com>
2024-04-08 16:09:46 +0900

    feat: 최종 우승자 출력 메소드 추가

--- src/main/kotlin/racingcar/view/Output.kt
+++ src/main/kotlin/racingcar/view/Output.kt
@@ -5,6 +5,7 @@ class Output {
         const val START_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
         const val ATTEMPT_MESSAGE = "시도할 횟수는 몇 회인가요?"
         const val RESULT_MESSAGE = "실행 결과"
+        const val WINNER_MESSAGE = "최종 우승자 : "
         const val COLON = " : "
         const val POSITION_SIGN = "-"
         const val SEPARATOR = ", "
@@ -26,7 +27,8 @@ class Output {
         println("$car$COLON" + POSITION_SIGN.repeat(position))
     }
 
-    fun printWinner(winner: List<String>) {
-        val result = winner.joinToString(SEPARATOR)
+    fun printWinner(winnerList: List<String>) {
+        val winner = winnerList.joinToString(SEPARATOR)
+        print("${WINNER_MESSAGE}${winner}")
     }
 }
