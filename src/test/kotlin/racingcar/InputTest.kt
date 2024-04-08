b3c6f2ef977b1acddb0cba267e7fe26c1d511ebf
younha00 <ayeon0@office.skhu.ac.kr>
2024-04-08 16:42:01 +0900

    Test: 자동차 이름의 중복을 검증하는 테스트 추가

--- src/test/kotlin/racingcar/InputTest.kt
+++ src/test/kotlin/racingcar/InputTest.kt
@@ -12,15 +12,21 @@ import racingcar.model.Car
 class InputTest : NsTest() {
     @ParameterizedTest
     @ValueSource(strings = ["pobi", "woni", "jun"])
-    fun `참여자 이름으로 차 생성`(participant: String) {
+    fun `참여자 이름으로 자동차 생성`(participant: String) {
         val car = Car(participant)
         Assertions.assertNotNull(car)
         Assertions.assertEquals(participant, car.name)
     }
     @Test
-    fun `이름에 대한 예외 처리`() {
+    fun `참여자의 이름은 5자 이하야한다`() {
         assertSimpleTest {
-            assertThrows<IllegalArgumentException> { runException("pobi,javaji", "1") }
+            assertThrows<IllegalArgumentException> { runException("pobi,javaii", "1") }
+        }
+    }
+    @Test
+    fun `자동차의 이름은 중복될 수 없다`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("pobi,pobi", "1") }
         }
     }
     public override fun runMain() {
