de1e4e5237d6e22a6895dd7b088d7fa94839f018
younha00 <ayeon0@office.skhu.ac.kr>
2024-04-08 18:13:15 +0900

    Refactor: View 클래스 기존 class로 변경

--- src/main/kotlin/racingcar/view/OutputView.kt
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -4,7 +4,7 @@ import racingcar.model.Car
 const val MESSAGE_OF_RACE_RESULT: String = "실행 결과 "
 
 
-object OutputView {
+class OutputView {
     fun printRaceResultMessage() {
         println()
         println(MESSAGE_OF_RACE_RESULT)
