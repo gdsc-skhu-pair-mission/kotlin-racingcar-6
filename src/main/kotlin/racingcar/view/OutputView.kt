714f41d7c3a6762f57fe1aec545581680759dd41
younha00 <ayeon0@office.skhu.ac.kr>
2024-04-08 17:36:52 +0900

    Feat: 유저에게 결과를 출력하는 OutputView 구현: * 한 번의 게임 결과를 출력 * 우승자들의 이름을 출력

---
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -0,0 +1,24 @@
+package racingcar.view
+
+import racingcar.model.Car
+const val MESSAGE_OF_RACE_RESULT: String = "실행 결과 "
+
+
+class OutputView {
+    fun printRaceResultMessage() {
+        println()
+        println(MESSAGE_OF_RACE_RESULT)
+    }
+
+    fun printRaceResult(cars: List<Car>) {
+        cars.forEach {
+            println("${it.name.trim()} : ${"-".repeat(it.distance)}")
+        }
+        println()
+    }
+
+    fun printWinners(winnerNames: List<String>) {
+        println("최종 우승자 : ${winnerNames.joinToString(", ")}")
+    }
+
+}
