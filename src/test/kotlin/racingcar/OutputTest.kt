c569fbcd2c9501a27cf61f6bb59d6f40647665ab
cucumber99 <dlsguq1@gmail.com>
2024-04-15 17:42:23 +0900

    test: 출력 테스트 수정

--- src/test/kotlin/racingcar/OutputTest.kt
+++ src/test/kotlin/racingcar/OutputTest.kt
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.DisplayName
 import java.io.ByteArrayOutputStream
 import java.io.PrintStream
 import racingcar.view.Output
@@ -11,7 +12,6 @@ import racingcar.view.Output
 class OutputTest {
     private val standardOut = System.out
     private val outputStreamCaptor = ByteArrayOutputStream()
-    private val output = Output()
 
     @BeforeEach
     fun setUp() {
@@ -24,35 +24,40 @@ class OutputTest {
     }
 
     @Test
-    fun `시작 메시지 출력 테스트`() {
-        output.printStartMessage()
+    @DisplayName("시작 메시지 출력 테스트")
+    fun `printStartMessage() should print start message`() {
+        Output.printStartMessage()
         assertEquals(Output.START_MESSAGE + System.lineSeparator(), outputStreamCaptor.toString())
     }
 
     @Test
-    fun `시도 횟수 요청 메시지 출력 테스트`() {
-        output.printAttemptMessage()
+    @DisplayName("시도 횟수 요청 메시지 출력 테스트")
+    fun `printAttemptMessage() should print attempt message`() {
+        Output.printAttemptMessage()
         assertEquals(Output.ATTEMPT_MESSAGE + System.lineSeparator(), outputStreamCaptor.toString())
     }
 
     @Test
-    fun `실행 결과 메시지 출력 테스트`() {
-        output.printResultMessage()
+    @DisplayName("실행 결과 메시지 출력 테스트")
+    fun `printResultMessage() should print result message`() {
+        Output.printResultMessage()
         assertEquals(System.lineSeparator() + Output.RESULT_MESSAGE + System.lineSeparator(), outputStreamCaptor.toString())
     }
 
     @Test
-    fun `자동차 위치 출력 테스트`() {
+    @DisplayName("자동차 위치 출력 테스트")
+    fun `printPosition() should print car name, position signs`() {
         val car = "pobi"
         val position = 3
-        output.printPosition(car, position)
+        Output.printPosition(car, position)
         assertEquals("$car${Output.COLON}${Output.POSITION_SIGN.repeat(position)}" + System.lineSeparator(), outputStreamCaptor.toString())
     }
 
     @Test
-    fun `최종 우승자 출력 테스트`() {
+    @DisplayName("최종 우승자 출력 테스트")
+    fun `printWinner() should print winner message followed by winner names separated by comma`() {
         val winners = listOf("pobi", "jun")
-        output.printWinner(winners)
+        Output.printWinner(winners)
         assertEquals("${Output.WINNER_MESSAGE}${winners.joinToString(Output.SEPARATOR)}", outputStreamCaptor.toString())
     }
 }
